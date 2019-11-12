64bf61548101e0500cf3e0c33369ba146627e36b
Sergey Murzin <sergey.murzin@dataart.com>
2019-11-12 02:24:29 +0200

    add working proj

--- src/App.js
+++ src/App.js
@@ -3,26 +3,59 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      {/*<header className="App-header">*/}
-      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.js</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
+    return (
+        <div className="App">
+            <div className="uk-container">
+                <CommonBtn handler={handleAdd} type='default' message='Add'/>
+                <CommonBtn handler={handleCopy} type='primary' message='Copy'/>
+                <CommonBtn handler={handleClear} type='secondary' message='Clear'/>
+            </div>
+        </div>
+    );
+}
 
-      <div className="uk-container">Hello world!</div>
-    </div>
-  );
+var locations = [];
+
+function handleAdd() {
+    locations.push(window.location.href)
+}
+
+const copyToClipboard = str => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
+
+function handleCopy() {
+    let text = locations.join(",");
+
+    // chrome.tabs.getSelected(null,function(tab) {
+    //     text = tab.url;
+    // });
+
+    copyToClipboard(text);
+    console.log('title: ' + document.title);
+}
+
+function handleClear() {
+    locations.length = 0;
+}
+
+class CommonBtn extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    render() {
+        return <button onClick={this.props.handler()}
+                       className={'uk-button uk-button-' + this.props.type + ' uk-width-1-1 uk-margin-small-bottom'}>{this.props.message}</button>;
+    }
 }
 
 export default App;

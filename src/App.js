45548876770631dead2ebb144aff31adb0749646
Sergey Murzin <sergey.murzin@dataart.com>
2019-11-13 21:05:57 +0200

    added animations

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ class App extends React.Component {
         return (
             <div className="App">
                 <div className="uk-container">
-                    <CommonBtn handler={e=>sender.add()} type='default' message='Add'/>
-                    <CommonBtn handler={e=>sender.copy()} type='primary' message='Copy'/>
-                    <CommonBtn handler={e=>sender.clear()} type='secondary' message='Clear'/>
+                    <CommonBtn handler={e => sender.add()} type='default' message='Add'/>
+                    <CommonBtn handler={e => sender.copy()} type='primary' message='Copy'/>
+                    <CommonBtn handler={e => sender.clear()} type='secondary' message='Clear'/>
                 </div>
             </div>
         );
@@ -22,9 +22,26 @@ class App extends React.Component {
 }
 
 class CommonBtn extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {click: false};
+    }
+
+    handleClick() {
+        this.setState({click: true});
+        setTimeout(() => {
+            this.setState({click: false});
+        }, 2000);
+
+        this.props.handler()
+    }
+
     render() {
-        return <button onClick={e=>this.props.handler()}
-                       className={'uk-button uk-button-' + this.props.type + ' uk-width-1-1 uk-margin-small-bottom'}>{this.props.message}</button>;
+        return <div className={this.state.click === true ? ' uk-animation-slide-right uk-animation-reverse' : ''}>
+            <button onClick={e => this.handleClick()}
+                    className={'uk-button uk-button-' + this.props.type + ' uk-width-1-1 uk-margin-small-bottom'}>{this.props.message}</button>
+        </div>;
     }
 }
 
